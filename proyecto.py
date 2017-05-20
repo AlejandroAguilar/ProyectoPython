aaaeff8cfd7f3fcfdb9314c30ecb2d8c8aae51b8
Galm52 <mauricio.garcia.cs@gmail.com>
2017-05-20 18:36:31 -0500

    V 1.1.1.1
    version

--- proyecto.py
+++ proyecto.py
@@ -1,16 +1,22 @@
 import web
         
 urls = (
-    '/(.*)', 'hello'
+    #'/(.*)', 'hello'
+    '/', 'Index'
 )
 app = web.application(urls, globals())
 render = web.template.render('templates/')
+
+class Index:
+	def GET( self ):
+		return render.index()
+
 
 class hello:        
     def GET(self, name):
         if not name: 
             name = 'World'
         return 'Hello, ' + name + '!'
-        
+
 if __name__ == "__main__":
     app.run()
