573ce5cc2648cafe20ffc51e39ecee7ea349730c
Alejandro Aguilar <downvieri@gmail.com>
2017-05-21 14:50:32 -0500

    Adding styles
    Adding styles to system.

--- static/js/eventos.js
+++ static/js/eventos.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-
     $( "#button_submit" ).click(function() {
 
     	var	usuario = $("#usuario").val();
@@ -10,32 +9,23 @@ $( document ).ready(function() {
 
 	  if (true) {
 
-	  		console.log("Envio inf");
-	  		console.log(usuario);
-	  		console.log(pass);
-
 		    $.ajax({
 			  method: "POST",
 			  url: "/",
 			  data: { "usuario": usuario, "pass": pass },
 			  dataType: 'json',
 			  success:function(data) {
-			    console.log("Exito");
-			    console.log(data);
-			    console.log("resultado: "+data.resultado);
 
 			    if ( data.resultado ) {
 			    	// Se puede quitar el timeout
 			     	setTimeout(function(){
-			     		console.log('Rediccion')
                         window.location.href = "/inicio";
                    	}
-                    ,1800);
+                    ,0);
 			    }
 
 			  },
 		      error: function(xhr, textStatus, error) {
-			      console.log("Error");
 
 			      console.log(xhr.statusText);
 			      console.log(textStatus);
